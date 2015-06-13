749dd37af3d541cd90de3b45c7a2d0bccb6f33a5
Ben Dudson <bd512@york.ac.uk>
2015-06-13 08:19:17 +0100

    MMS/elm-pb adding analysis scripts
    To avoid copying data from Archer, analysis
    script calculates error norms and pickles them.
    runtest then reads these files for plotting.

---
+++ examples/MMS/elm-pb/analyse.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+# Python script to analyse MMS test output
+
+from boutdata import collect
+
+from numpy import sqrt, max, abs, mean, array
+
+import pickle
+
+nxlist = [8, 16, 32, 64]
+varlist = ["P", "Psi", "U"]
+
+tind = -1
+
+error_2 = {}
+error_inf = {}
+
+for nx in nxlist:
+    directory = "grid%d" % nx
+    
+    for var in varlist:
+        # Collect data
+        E = collect("E_"+var, tind=[tind,tind], info=False, path=directory)
+        E = E[:,2:-2, :,:]
+        
+        # Average error over domain
+        l2 = sqrt(mean(E**2))
+        linf = max(abs( E ))
+    
+        error_2[var] = l2
+        error_inf[var] = linf
+
+        print("%s : l-2 %f l-inf %f" % (var, l2, linf))
+    
+    # Save data
+    with open(directory+".pkl", "wb") as output:
+        pickle.dump(nx, output)
+        pickle.dump(error_2, output)
+        pickle.dump(error_inf, output)
+    
